dd4dfa6d4edea79b18a40c14cfc9d61bfb92c232
Jiapeng <791954776@qq.com>
2024-06-13 17:06:07 +0800

    add CARLA

---
+++ CARLA/Clear-Test-Autonomous-Driving-in-Carla/simulation/connection.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import glob
+
+try:
+    sys.path.append(glob.glob('./carla/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    print('Couldn\'t import Carla egg properly')
+
+import carla
+from simulation.settings import PORT, TIMEOUT, HOST
+
+class ClientConnection:
+    def __init__(self, town):
+        self.client = None
+        self.town = town
+
+    def setup(self):
+        try:
+
+            # Connecting to the  Server
+            self.client = carla.Client(HOST, PORT)
+            self.client.set_timeout(TIMEOUT)
+            self.world = self.client.load_world(self.town)
+            # self.world.set_weather(carla.WeatherParameters.CloudyNoon)
+            #可用环境：ClearNoon, CloudyNoon, WetNoon, WetCloudyNoon, SoftRainNoon, MidRainyNoon, HardRainNoon, ClearSunset, CloudySunset, WetSunset, WetCloudySunset, SoftRainSunset, MidRainSunset, HardRainSunset.
+            self.world.set_weather(carla.WeatherParameters.CloudyNoon)
+
+            # weather = carla.WeatherParameters(
+            #     cloudiness=80.0,
+            #     precipitation=30.0,
+            #     sun_altitude_angle=70.0,
+            #     fog_density = 2000.0,
+            #     fog_distance = 20000)
+            # self.world.set_weather(weather)
+
+            return self.client, self.world
+
+        except Exception as e:
+            print(
+                'Failed to make a connection with the server: {}'.format(e))
+            self.error()
+
+    # An error method: prints out the details if the client failed to make a connection
+    def error(self):
+
+        print("\nClient version: {}".format(
+            self.client.get_client_version()))
+        print("Server version: {}\n".format(
+            self.client.get_server_version()))
+
+        if self.client.get_client_version != self.client.get_server_version:
+            print(
+                "There is a Client and Server version mismatch! Please install or download the right versions.")
